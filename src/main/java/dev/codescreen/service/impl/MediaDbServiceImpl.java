1ae590553eb47ce02e4d648029c6f10327d011c3
Neil Sherwin Espolong <mcub3x_spl0ngk@Neils-MacBook-Pro.local>
2024-01-29 11:28:33 +0800

    exception handler

--- src/main/java/dev/codescreen/service/impl/MediaDbServiceImpl.java
+++ src/main/java/dev/codescreen/service/impl/MediaDbServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import dev.codescreen.client.MediaDbApiClient;
+import dev.codescreen.handler.MediaDbInternalServerException;
 import dev.codescreen.model.MediaObject;
 import dev.codescreen.model.MediaDbListResponse;
 import dev.codescreen.model.MediaDbResponse;
@@ -23,7 +24,7 @@ public class MediaDbServiceImpl implements MediaDbService {
 	}
 
 	@Override
-	public List<MediaObject> searchMovies(String title) {
+	public List<MediaObject> searchMovies(String title) throws MediaDbInternalServerException {
 
 		MediaDbListResponse response = client.searchMovie(title);
 
@@ -34,14 +35,14 @@ public class MediaDbServiceImpl implements MediaDbService {
 	}
 
 	@Override
-	public MediaObject searchMovie(Integer id) {
+	public MediaObject searchMovie(Integer id) throws MediaDbInternalServerException {
 		MediaDbResponse response = client.searchMovieById(id);
 
 		return MediaMapper.toMedia(response);
 	}
 
 	@Override
-	public List<MediaObject> searchShows(String name) {
+	public List<MediaObject> searchShows(String name) throws MediaDbInternalServerException {
 		MediaDbListResponse response = client.searchShows(name);
 
 		List<ResultObject> results = response.getTotalResults() > 10 ? response.getResults().subList(0, 9)
@@ -51,7 +52,7 @@ public class MediaDbServiceImpl implements MediaDbService {
 	}
 
 	@Override
-	public MediaObject searchShow(Integer id) {
+	public MediaObject searchShow(Integer id) throws MediaDbInternalServerException {
 		MediaDbResponse response = client.searchShowById(id);
 
 		return MediaMapper.toMedia(response);
